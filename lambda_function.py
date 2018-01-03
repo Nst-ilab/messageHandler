0c30b14e7c6f040048901778e4dece9e02d12dca
EC2 Default User <ec2-user@ip-10-0-0-12.ap-southeast-1.compute.internal>
2018-01-03 15:54:17 +0000

    add mr.oumu

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,71 @@
+# coding: UTF-8
+from __future__ import print_function
+
+import boto3,os
+import json,logging,re
+import urllib.request
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info('Loading function')
+
 def lambda_handler(event, context):
-    # TODO implement
-    return ''
+
+    logger.info("Received event: " + json.dumps(event, indent=2))
+    
+    '''
+    body_object = json.loads(event["body"])
+    messageText = body_object["events"][0]["message"]["text"]
+    
+    if messageText.find("天気") >= 0 :
+        # boto3でLambdaを使う宣言をします
+        clientLambda = boto3.client('lambda')
+        # Calleeを実行します
+        clientLambda.invoke(
+            # Calleeのarnを指定
+            FunctionName='i-lab-weather-service',
+            # RequestResponse = 同期、Event = 非同期 で実行できます
+            InvocationType='Event',
+            # 引数をJSON形式で渡します
+            Payload=json.dumps(event).encode("UTF-8")
+        )
+    '''
+    
+    # LINE側のメッセージAPIにリクエストを送ることで返信とする
+    
+    lineMessage = json.loads(event["body"])
+    logger.info( json.dumps(lineMessage, indent=2))
+    
+    url = "https://api.line.me/v2/bot/message/reply"
+    method = "POST"
+    # ヘッダ設定 LINE用認証トークンは別途管理
+    headers = {
+        'Authorization':'Bearer ' + os.environ.get('LINE_LONGTIME_TOKEN') ,
+        'Content-Type': 'application/json'
+    }
+
+    params = {
+        "replyToken": lineMessage["events"][0]["replyToken"] ,
+        "messages" : [
+            {
+                "type": "text" ,
+                "text": lineMessage["events"][0]["message"]["text"]
+            }
+        ]
+    }
+    json_data = json.dumps(params).encode("utf-8")
+    request = urllib.request.Request(url, data=json_data, headers=headers, method=method)
+
+    with urllib.request.urlopen(request) as response:
+        logger.info(response.read().decode("utf-8"))
+
+    
+    # API-Gateway側へのレスポンス
+    return {
+        'statusCode': 200,
+        'body': "応答受けたよ！",
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+    }
